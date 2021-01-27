3e57fb7ac91dafcbadc986845368b01109e3e764
Jacek <ja_cek@gmx.com>
2021-01-27 14:23:17 +0100

    app.js update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -177,8 +177,15 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-    console.log(battleLog);
-}
+        let i = 0;
+        for (const logEntry of battleLog) {
+        console.log(`#${i}`);
+        for (const key in logEntry) {
+            console.log(`${key} => ${logEntry[key]}`);
+        }
+        i++
+    }
+  }
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
